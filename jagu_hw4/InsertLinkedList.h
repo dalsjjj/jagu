2e2af84d3316477c28466269f55cd8da8a38e9a6
dalsjjj <mmssjj0403@naver.com>
2026-04-01 23:36:37 +0900

    Add files via upload

---
+++ jagu_hw4/InsertLinkedList.h
@@ -0,0 +1,17 @@
+#pragma once
+typedef struct ListNode {
+	char data[4];
+	struct ListNode* link;
+} listNode;
+
+typedef struct {
+	listNode* head;
+} linkedList_h;
+
+linkedList_h* createLinkedList_h(void);
+void freeLinkedList_h(linkedList_h* L);
+void printList_h(linkedList_h* L);
+void insertFirstNode_h(linkedList_h* L, char* x);
+void insertMiddleNode_h(linkedList_h* L, listNode* pre, char* x);
+void insertLastNode_h(linkedList_h* L, char* x);
+
